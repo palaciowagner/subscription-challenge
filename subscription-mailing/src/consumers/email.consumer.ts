81f34bd668ceae592d66e287c9d5562171b4408e
mahal <palaciowagner@gmail.com>
2022-03-21 00:06:41 -0300

    [mailing] Add Docker configuration for subscription-mailing

--- subscription-mailing/src/consumers/email.consumer.ts
+++ subscription-mailing/src/consumers/email.consumer.ts
@@ -2,7 +2,10 @@ import { KAFKA_EMAIL_SENDER_TOPIC_NAME } from "@/config";
 import { KafkaConsumer } from "@/connections/kafka.connection";
 import { MessageHandler } from "@/interfaces/messageHandler.interface";
 
-export default class EmailConsumer extends KafkaConsumer implements MessageHandler {
+export default class EmailConsumer
+  extends KafkaConsumer
+  implements MessageHandler
+{
   constructor() {
     super(KAFKA_EMAIL_SENDER_TOPIC_NAME);
   }
